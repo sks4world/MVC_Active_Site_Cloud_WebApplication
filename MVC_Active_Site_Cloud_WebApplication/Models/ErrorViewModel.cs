dd694012367c5de8085a0484af13835a570e225b
sks4world <minameissri@gmail.com>
2019-11-18 17:49:07 -0500

    Add project files.

---
+++ MVC_Active_Site_Cloud_WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_Active_Site_Cloud_WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
