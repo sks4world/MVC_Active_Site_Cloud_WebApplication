dd705a5bd4bcc13146c9b6dc04941c01fd643844
sks4world <minameissri@gmail.com>
2019-11-20 00:59:46 -0500

    Views, html files, css files added

---
+++ MVC_Active_Site_Cloud_WebApplication/Views/Home/foodrecall.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MVC_Active_Site_Cloud_WebApplication.Views.Home
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
